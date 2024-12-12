65e9d02cb196106fd60952f00b04bf205a125b8c
PrzemyslawBorczak1 <przemyslawborczak@gmail.com>
2024-12-12 17:51:28 +0100

    Initial commit

---
+++ Chess/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is gona be a mini project in which i will try to create chess game and mayby implement simple chess engine.");
+            Console.WriteLine("The inspiration comes from thi yt video https://youtu.be/U4ogK0MIzqk?si=VQ_55VoRpDBQ4g10");
+        }
+    }
+}
